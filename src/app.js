6e0e0ccb9de14e7fe55077fe1479a7e6f520ef7a
sandikshitdas25 <sandikshitdas@gmail.com>
2024-10-17 12:16:26 +0530

    add comment

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 //urlencoded use for decoding the url example like spaces are replaced with %20
 //extended use for nested object (not necessary)
 
-app.use(express.static("public")) //any images videos that are coming are stored at public file(public is a file name which can be any)
+app.use(express.static("public")) //any images videos that are coming are stored at public file(public is a file name which can be any), which made file accessible from anywhere
 
 app.use(cookieParser())   //using this middleware we gave access to cookies which is used in auth.middleware.js
 
